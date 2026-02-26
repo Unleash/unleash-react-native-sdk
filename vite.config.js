4fba3827a84a2248400284a3211ef52de93dcdb5
Nuno Góis <github@nunogois.com>
2026-02-26 09:44:11 +0000

    chore: fix build, release workflow (#12)
    * chore: fix build, release workflow
    
    * v1.0.0-alpha.4
    
    * chore: fix repository url
    
    * v1.0.0-alpha.5
    
    * chore: clean up vite.config.js
    
    * chore: future-proof vite.config.js
    
    * chore: adjust peer dependency
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <>

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
 export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'umd'],
-      name: 'UnleashReactNative',
+      formats: ['es'],
       fileName: 'unleash-react-native'
     },
     rollupOptions: {
-      external: ['react', 'unleash-proxy-client'],
-      output: {
-        exports: 'named',
-        globals: {
-          react: 'react'
-        }
-      }
+      external: [
+        ...Object.keys(pkg.peerDependencies || {}),
+        ...Object.keys(pkg.dependencies || {})
+      ]
     }
   },
   plugins: [dts()],
